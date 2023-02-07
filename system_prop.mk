07e593c47b5591766a01a0ef6c80d3f8ed57d85f
SamarV-121 <samarvispute121@pm.me>
2023-02-07 12:13:21 +0100

    msm8974-common: Disable HWC composition strategy prediction
    Change-Id: Id45165c604c825bc55a324e7dc8f3dcd7a3f25ee

--- system_prop.mk
+++ system_prop.mk
@@ -59,6 +59,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
     debug.sf.hw=1 \
     debug.renderengine.backend=threaded \
+    debug.sf.predict_hwc_composition_strategy=0 \
     dev.pm.dyn_samplingrate=1 \
     persist.hwc.mdpcomp.enable=true \
     ro.opengles.version=196608 \
